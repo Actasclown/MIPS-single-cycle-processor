d88bc198d7b08544dc52fe96efcba2912a2d57df
Youjia Li <yll6162@bane.ece.northwestern.edu>
2022-12-04 01:43:33 -0600

    analyzed two dat files

--- bin/xcelium.d/run.lnx8664.18.09.d/.timestamp.ts
+++ bin/xcelium.d/run.lnx8664.18.09.d/.timestamp.ts
@@ -11,7 +11,7 @@
 1668156004 /home/yhi4324/CE361/alu_design/test/sltu_32_tb.v
 1668152350 /home/yhi4324/CE361/alu_design/test/srl_32_tb.v
 1668198701 /home/yhi4324/CE361/alu_design/test/mux8_1_32_tb.v
-1669877147 /home/yll6162/CS361/proj_processor/MIPS-single-cycle-processor/test/signext_32_tb.v
+1670129232 /home/yll6162/CS361/proj_processor/MIPS-single-cycle-processor/test/signext_32_tb.v
 1668142985 /home/yhi4324/CE361/alu_design/test/sub_32_tb.v
 1668148924 /home/yhi4324/CE361/alu_design/test/mux32_1_tb.v
 1669939834 /home/yll6162/CS361/proj_processor/MIPS-single-cycle-processor/test/pc_head_32_tb.v
